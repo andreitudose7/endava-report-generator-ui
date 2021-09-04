3582b119d44003522a461b211f38295fb32b4066
Andrei Tudose <andreitudose7@gmail.com>
2021-09-04 11:34:57 +0300

    Logic splitted into Components

--- src/App.vue
+++ src/App.vue
@@ -39,145 +39,20 @@
   <section v-for="(category, categoryIndex) in formValues.evaluation" :key="category.areaName">
     <div style="display: flex; justify-content: space-between; align-items: center;">
       <h3> {{ category.areaName }} </h3>
-
-      <div class="select" tabindex="1">
-        <input
-          class="selectopt"
-          value="-1"
-          :name="category.areaName + '-evaluation-dd'"
-          type="radio"
-          :id="category.areaName + '-evaluation-option-None'"
-          v-model="formValues.evaluation[categoryIndex].points"
-        />
-        <label :for="category.areaName + '-evaluation-option-None'" class="option">None</label>
-        <input
-          class="selectopt"
-          value="0"
-          :name="category.areaName + '-evaluation-dd'"
-          type="radio"
-          :id="category.areaName + '-evaluation-option-Beginner'"
-          v-model="formValues.evaluation[categoryIndex].points"
-        />
-        <label :for="category.areaName + '-evaluation-option-Beginner'" class="option">Beginner</label>
-        <input
-          class="selectopt"
-          value="1"
-          :name="category.areaName + '-evaluation-dd'"
-          type="radio"
-          :id="category.areaName + '-evaluation-option-Intermediate'"
-          v-model="formValues.evaluation[categoryIndex].points"
-        />
-        <label :for="category.areaName + '-evaluation-option-Intermediate'" class="option">Intermediate</label>
-        <input
-          class="selectopt"
-          value="2"
-          :name="category.areaName + '-evaluation-dd'"
-          type="radio"
-          :id="category.areaName + '-evaluation-option-Advanced'"
-          v-model="formValues.evaluation[categoryIndex].points"
-        />
-        <label :for="category.areaName + '-evaluation-option-Advanced'" class="option">Advanced</label>
-      </div>
+      <RatingDropdown
+        :widget-id="category.areaName + '-evaluation-dd'"
+        v-model="formValues.evaluation[categoryIndex].points"
+      />
     </div>
 
     <div v-for="(checkpoints, checkpointIndex) in category.checkpoints" :key="checkpoints.title" style="display: flex; justify-content: space-between;">
       <p> {{ checkpoints.title }} </p>
-      
-      <div id="full-stars-example">
-        <div class="rating-group">
-          <input
-            class="rating__input rating__input--none"
-            :name="'rating' + categoryIndex + '-' + checkpointIndex"
-            :id="'rating-0-' + categoryIndex + '-' + checkpointIndex"
-            value="-1"
-            type="radio"
-            v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
-          />
-          <label
-            aria-label="Not evaluated"
-            :class="{
-              'rating__label': true,
-              'active': formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points == -1
-            }"
-            :for="'rating-0-' + categoryIndex + '-' + checkpointIndex"
-          >
-            <i class="rating__icon rating__icon--none fa fa-ban"></i>
-          </label>
-          <input
-            class="rating__input"
-            :name="'rating' + categoryIndex + '-' + checkpointIndex"
-            :id="'rating-1-' + categoryIndex + '-' + checkpointIndex"
-            value="0"
-            type="radio"
-            v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
-          />
-          <label
-            aria-label="None"
-            :class="{
-              'rating__label': true,
-              'active': formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points == 0
-            }"
-            :for="'rating-1-' + categoryIndex + '-' + checkpointIndex"
-          >
-            <i class="rating__icon rating__icon--star fa fa-frown"></i>
-          </label>
-          <input
-            class="rating__input"
-            :name="'rating' + categoryIndex + '-' + checkpointIndex"
-            :id="'rating-2-' + categoryIndex + '-' + checkpointIndex"
-            value="1"
-            type="radio"
-            v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
-          />
-          <label
-            aria-label="Beginner"
-            :class="{
-              'rating__label': true,
-              'active': formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points == 1
-            }"
-            :for="'rating-2-' + categoryIndex + '-' + checkpointIndex"
-          >
-            <i class="rating__icon rating__icon--star fa fa-meh"></i>
-          </label>
-          <input
-            class="rating__input"
-            :name="'rating' + categoryIndex + '-' + checkpointIndex"
-            :id="'rating-3-' + categoryIndex + '-' + checkpointIndex"
-            value="2"
-            type="radio"
-            v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
-          />
-          <label
-            aria-label="Intermediate"
-            :class="{
-              'rating__label': true,
-              'active': formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points == 2
-            }"
-            :for="'rating-3-' + categoryIndex + '-' + checkpointIndex"
-          >
-            <i class="rating__icon rating__icon--star fa fa-grin-alt"></i>
-          </label>
-          <input
-            class="rating__input"
-            :name="'rating' + categoryIndex + '-' + checkpointIndex"
-            :id="'rating-4-' + categoryIndex + '-' + checkpointIndex"
-            value="3"
-            type="radio"
-            v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
-          />
-          <label
-            aria-label="Advanced"
-            :class="{
-              'rating__label': true,
-              'active': formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points == 3
-            }"
-            :for="'rating-4-' + categoryIndex + '-' + checkpointIndex"
-          >
-            <i class="rating__icon rating__icon--star fa far fa-grin-wink"></i>
-          </label>
-        </div>
-      </div>
-
+      <RatingFaces
+        :widget-id="'rating-' + categoryIndex + '-' + checkpointIndex"
+        :category-index="categoryIndex"
+        :checkpoint-index="checkpointIndex"
+        v-model="formValues.evaluation[categoryIndex].checkpoints[checkpointIndex].points"
+      />
     </div>
   </section>
   <section>
@@ -201,9 +76,15 @@
 </template>
 
 <script>
+import RatingFaces from './components/RatingFaces.vue';
+import RatingDropdown from './components/RatingDropdown.vue';
 import evaluationForm from './interview-questions/evaluationFormat';
 export default {
   name: 'App',
+  components: {
+    RatingFaces,
+    RatingDropdown
+  },
   data() {
     return {
       interviewersDS: ['Andrei Tudose', 'Stefan Ioan Adrian', 'Marian Costel Brinza', 'Ionut Cristian Stoica'],
@@ -284,139 +165,4 @@ export default {
     border-color: black;
     font-weight: normal;
   }
-
-
-
-
-
-
-
-.select {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  width: 160px;
-  height: 40px;
-}
-
-.select .option {
-  padding:0 30px 0 10px;
-  min-height:40px;
-  display:flex;
-  align-items:center;
-  background:#eee;
-  border-top:#eee solid 1px;
-  position:absolute;
-  top:0;
-  width: 100%;
-  pointer-events:none;
-  order:2;
-  z-index:1;
-  transition:background .4s ease-in-out;
-  box-sizing:border-box;
-  overflow:hidden;
-  white-space:nowrap;
-}
-
-.select .option:hover {
-  background:#eee;
-}
-
-.select:focus .option {
-  position:relative;
-  pointer-events:all;
-}
-
-.select input {
-  opacity: 0;
-  position: absolute;
-  left: -99999px;
-}
-
-.select input:checked + label {
-  order: 1;
-  z-index:2;
-  background: #eee;
-  border-top:none;
-  position:relative;
-}
-
-.select input:checked + label:after {
-  content:'';
-  width: 0; 
-	height: 0; 
-	border-left: 5px solid transparent;
-	border-right: 5px solid transparent;
-	border-top: 5px solid white;
-  position:absolute;
-  right:10px;
-  top:calc(50% - 2.5px);
-  pointer-events:none;
-  z-index:3;
-}
-
-.select input:checked + label:before {
-  position:absolute;
-  right:0;
-  height: 40px;
-  width: 40px;
-  content: '';
-  background:#eee;
-}
-
-
-
-
-
-
-/* * Pure CSS star rating that works without reversing order * of inputs * ------------------------------------------------------- * NOTE: For the styling to work, there needs to be a radio * input selected by default. There also needs to be a * radio input before the first star, regardless of * whether you offer a 'no rating' or 0 stars option * * This codepen uses FontAwesome icons */ 
-#full-stars-example .rating-group {
-	display: inline-flex;
-}
-#full-stars-example .rating__icon {
-	pointer-events: none;
-}
-#full-stars-example .rating__input {
-	display: none;
-}
-#full-stars-example .rating__label {
-	cursor: pointer;
-	padding: 0 0.1em;
-	font-size: 2rem;
-}
-
-/* by default, the first radio - the one that is not star - should be gray - not selected */
-#full-stars-example .rating__icon--none {
-	color: #eee;
-}
-/* if the first radio - the one that is not star - is checked, then should be red */
-#full-stars-example .rating__input--none:checked + .rating__label .rating__icon--none {
-	color: red;
-}
-/* the default for the stars */
-#full-stars-example .rating__icon--star {
-	color: #eee;
-}
-#full-stars-example .rating__label:hover {
-  transition-duration: 0.2s;
-  transform: scale(1.2);
-}
-#full-stars-example .rating__input--none:hover + .rating__label .rating__icon--none {
-	color: red;
-}
-#full-stars-example .rating__label.active .rating__icon--none {
-	color: red !important;
-}
-#full-stars-example .rating__label[aria-label="None"].active .rating__icon--star {
-	color: #ff3838 !important;
-}
-#full-stars-example .rating__label[aria-label="Beginner"].active .rating__icon--star {
-	color: #ebe800 !important;
-}
-#full-stars-example .rating__label[aria-label="Intermediate"].active .rating__icon--star {
-	color: #6eff1d !important;
-}
-#full-stars-example .rating__label[aria-label="Advanced"].active .rating__icon--star {
-	color: #2dccff !important;
-} 
 </style>
---
+++ src/components/RatingFaces.vue
@@ -0,0 +1,166 @@
+<template>
+    <div class="rating-faces">
+        <div class="rating-group">
+            <input
+                class="rating__input rating__input--none"
+                :name="widgetId"
+                :id="'1st-rating-' + widgetId"
+                value="-1"
+                type="radio"
+                :checked="modelValue == -1"
+                @click="$emit('update:modelValue', $event.target.value)"
+            />
+            <label
+                aria-label="Not evaluated"
+                :class="{
+                    'rating__label': true,
+                    'active': modelValue == -1
+                }"
+                :for="'1st-rating-' + widgetId"
+            >
+                <i class="rating__icon rating__icon--none fa fa-ban"></i>
+            </label>
+            <input
+                class="rating__input"
+                :name="widgetId"
+                :id="'2nd-rating-' + widgetId"
+                value="0"
+                type="radio"
+                :checked="modelValue == 0"
+                @click="$emit('update:modelValue', $event.target.value)"
+            />
+            <label
+                aria-label="None"
+                :class="{
+                    'rating__label': true,
+                    'active': modelValue == 0
+                }"
+                :for="'2nd-rating-' + widgetId"
+            >
+                <i class="rating__icon rating__icon--star fa fa-frown"></i>
+            </label>
+            <input
+                class="rating__input"
+                :name="widgetId"
+                :id="'3rd-rating-' + widgetId"
+                value="1"
+                type="radio"
+                :checked="modelValue == 1"
+                @click="$emit('update:modelValue', $event.target.value)"
+            />
+            <label
+                aria-label="Beginner"
+                :class="{
+                    'rating__label': true,
+                    'active': modelValue == 1
+                }"
+                :for="'3rd-rating-' + widgetId"
+            >
+                <i class="rating__icon rating__icon--star fa fa-meh"></i>
+            </label>
+            <input
+                class="rating__input"
+                :name="widgetId"
+                :id="'4th-rating-' + widgetId"
+                value="2"
+                type="radio"
+                :checked="modelValue == 2"
+                @click="$emit('update:modelValue', $event.target.value)"
+            />
+            <label
+                aria-label="Intermediate"
+                :class="{
+                    'rating__label': true,
+                    'active': modelValue == 2
+                }"
+                :for="'4th-rating-' + widgetId"
+            >
+                <i class="rating__icon rating__icon--star fa fa-grin-alt"></i>
+            </label>
+            <input
+                class="rating__input"
+                :name="widgetId"
+                :id="'5th-rating-' + widgetId"
+                value="3"
+                type="radio"
+                :checked="modelValue == 3"
+                @click="$emit('update:modelValue', $event.target.value)"
+            />
+            <label
+                aria-label="Advanced"
+                :class="{
+                    'rating__label': true,
+                    'active': modelValue == 3
+                }"
+                :for="'5th-rating-' + widgetId"
+            >
+                <i class="rating__icon rating__icon--star fa far fa-grin-wink"></i>
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RatingFaces',
+    props: [
+        'widgetId',
+        'categoryIndex', 
+        'checkpointIndex', 
+        'modelValue'
+    ]
+}
+</script>
+
+<style>
+/* * Pure CSS star rating that works without reversing order * of inputs * ------------------------------------------------------- * NOTE: For the styling to work, there needs to be a radio * input selected by default. There also needs to be a * radio input before the first star, regardless of * whether you offer a 'no rating' or 0 stars option * * This codepen uses FontAwesome icons */ 
+.rating-faces .rating-group {
+	display: inline-flex;
+}
+.rating-faces .rating__icon {
+	pointer-events: none;
+}
+.rating-faces .rating__input {
+	display: none;
+}
+.rating-faces .rating__label {
+	cursor: pointer;
+	padding: 0 0.1em;
+	font-size: 2rem;
+}
+
+/* by default, the first radio - the one that is not star - should be gray - not selected */
+.rating-faces .rating__icon--none {
+	color: #eee;
+}
+/* if the first radio - the one that is not star - is checked, then should be red */
+.rating-faces .rating__input--none:checked + .rating__label .rating__icon--none {
+	color: red;
+}
+/* the default for the stars */
+.rating-faces .rating__icon--star {
+	color: #eee;
+}
+.rating-faces .rating__label:hover {
+  transition-duration: 0.2s;
+  transform: scale(1.2);
+}
+.rating-faces .rating__input--none:hover + .rating__label .rating__icon--none {
+	color: red;
+}
+.rating-faces .rating__label.active .rating__icon--none {
+	color: red !important;
+}
+.rating-faces .rating__label[aria-label="None"].active .rating__icon--star {
+	color: #ff3838 !important;
+}
+.rating-faces .rating__label[aria-label="Beginner"].active .rating__icon--star {
+	color: #ebe800 !important;
+}
+.rating-faces .rating__label[aria-label="Intermediate"].active .rating__icon--star {
+	color: #6eff1d !important;
+}
+.rating-faces .rating__label[aria-label="Advanced"].active .rating__icon--star {
+	color: #2dccff !important;
+} 
+</style>
